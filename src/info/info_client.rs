e62a62a2f36c9de8e109501accae127fc2d5f2a5
Joel Earps <joel.earps@swissborg.com>
2025-05-09 17:22:15 +0100

    fix: clone for info client

--- src/info/info_client.rs
+++ src/info/info_client.rs
@@ -116,7 +116,7 @@ pub enum InfoRequest {
     },
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct InfoClient {
     pub http_client: HttpClient,
     pub(crate) ws_manager: Option<WsManager>,
